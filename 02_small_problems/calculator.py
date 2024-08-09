c8e9e68fa7674b28e7aaad9bd591b88de434147a
Raymond Saade <raymond.saade@gmail.com>
2024-08-09 17:02:43 -0400

    added floating point support

--- 02_small_problems/calculator.py
+++ 02_small_problems/calculator.py
@@ -1,56 +1,56 @@
 import json
 with open('calculator_messages.json', 'r') as file:
-    data = json.load(file)
+    MESSAGES = json.load(file)
 
 def prompt(message):
     print(f"==> {message}")
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
-prompt(data['welcome'])
+prompt(MESSAGES['welcome'])
 while True:
-    prompt(data['first'])
+    prompt(MESSAGES['first'])
     number1 = input()
 
     while invalid_number(number1):
-        prompt(data['invalid'])
+        prompt(MESSAGES['invalid'])
         number1 = input()
 
-    prompt(data['second'])
+    prompt(MESSAGES['second'])
     number2 = input()
 
     while invalid_number(number2):
-        prompt(data['invalid'])
+        prompt(MESSAGES['invalid'])
         number2 = input()
 
 
-    prompt(data['operation'])
+    prompt(MESSAGES['operation'])
 
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt(data['invalid op'])
+        prompt(MESSAGES['invalid op'])
         operation = input()
 
     match operation:
         case '1':
-            output = int(number1) + int(number2)
+            output = float(number1) + float(number2)
         case '2':
-            output = int(number1) - int(number2)
+            output = float(number1) - float(number2)
         case '3':
-            output= int(number1) * int(number2)
+            output= float(number1) * float(number2)
         case '4':
-            output = int(number1) / int(number2)
+            output = float(number1) / float(number2)
 
-    prompt(f"{data['result']} {output}")
+    prompt(f"{MESSAGES['result']} {output}")
     
-    prompt(data['continue'])
+    prompt(MESSAGES['continue'])
     answer = input()
     if answer and answer[0].lower() != 'y':
         break
