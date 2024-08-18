3ae22db7911d9908d341ddb05ef5861214fa5a7c
Raymond Saade <raymond.saade@gmail.com>
2024-08-18 01:20:20 -0400

    added abbreviated input and fixed spelling bug

--- 02_small_problems/rock_paper_scissors.py
+++ 02_small_problems/rock_paper_scissors.py
@@ -7,6 +7,20 @@ def prompt(message):
     print(f"==> {message}")
 
 
+def process_choice(player_choice: str):
+    player_choice = player_choice.lower()
+    if player_choice.startswith("r"):
+        return "rock"
+    if player_choice.startswith("p"):
+        return "paper"
+    if player_choice.startswith("sc"):
+        return "scissors"
+    if player_choice.startswith("l"):
+        return "lizard"
+    if player_choice.startswith("sp"):
+        return "spock"
+    return ""
+
 def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
 
@@ -20,7 +34,7 @@ def display_winner(player, computer):
         prompt("You win!")
     elif (
         (player == "rock" and computer in ["paper", "spock"])
-        or (player == "paper" and computer in ["scissors", "lizzard"])
+        or (player == "paper" and computer in ["scissors", "lizard"])
         or (player == "scissors" and computer in ["rock", "spock"])
         or (player == "lizard" and computer in ["rock", "scissors"])
         or (player == "spock" and computer in ["paper", "lizard"])
@@ -29,14 +43,15 @@ def display_winner(player, computer):
     else:
         prompt("It's a tie!")
 
+
 # Game loop
 while True:
     prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
-    choice = input()
+    choice = process_choice(input())
 
     while choice not in VALID_CHOICES:
         prompt("That's not a valid choice")
-        choice = input()
+        choice = process_choice(input())
 
     computer_choice = random.choice(VALID_CHOICES)
     display_winner(choice, computer_choice)
