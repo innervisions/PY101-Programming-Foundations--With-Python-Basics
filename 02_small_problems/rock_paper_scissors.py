94c20858a15e9e54ee8d549ebfa9228c63df23c1
Raymond Saade <raymond.saade@gmail.com>
2024-08-18 00:23:40 -0400

    added lizard spock

--- 02_small_problems/rock_paper_scissors.py
+++ 02_small_problems/rock_paper_scissors.py
@@ -1,24 +1,35 @@
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
+
 
 def prompt(message):
     print(f"==> {message}")
 
+
 def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
 
-    if ((player == "rock" and computer == "scissors") or
-        (player == "paper" and computer == "rock") or
-        (player == "scissors" and computer == "paper")):
+    if (
+        (player == "rock" and computer in ["scissors", "lizard"])
+        or (player == "paper" and computer in ["rock", "spock"])
+        or (player == "scissors" and computer in ["paper", "lizard"])
+        or (player == "lizard" and computer in ["paper", "spock"])
+        or (player == "spock" and computer in ["rock", "scissors"])
+    ):
         prompt("You win!")
-    elif ((player == "rock" and computer == "paper") or
-        (player == "paper" and computer == "scissors") or
-        (player == "scissors" and computer == "rock")):
+    elif (
+        (player == "rock" and computer in ["paper", "spock"])
+        or (player == "paper" and computer in ["scissors", "lizzard"])
+        or (player == "scissors" and computer in ["rock", "spock"])
+        or (player == "lizard" and computer in ["rock", "scissors"])
+        or (player == "spock" and computer in ["paper", "lizard"])
+    ):
         prompt("Computer wins!")
     else:
         prompt("It's a tie!")
 
+# Game loop
 while True:
     prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
     choice = input()
@@ -33,11 +44,11 @@ while True:
     prompt("Do you want to play again (y/n)?")
     answer = input().lower()
     while True:
-        if answer.startswith('n') or answer.startswith('y'):
+        if answer.startswith("n") or answer.startswith("y"):
             break
 
         prompt('Please enter "y" or "n".')
         answer = input().lower()
 
-    if answer[0] == 'n':
+    if answer[0] == "n":
         break
