d30409fa7bcf9ffc9c1975b5c8bf45cc40c674ab
Raymond Saade <raymond.saade@gmail.com>
2024-08-19 18:33:04 -0400

    Reapply "implemented matches"
    This reverts commit 1bc783d3d0ca71f1368862b1f9a4c58e6ee5270a.

--- 02_small_problems/rock_paper_scissors.py
+++ 02_small_problems/rock_paper_scissors.py
@@ -39,40 +39,84 @@ def process_choice(player_choice: str) -> str:
     return ""
 
 
-def display_winner(player, computer):
-    prompt(f"You chose {player}, computer chose {computer}")
-
+def determine_winner(player: str, computer: str) -> str:
     if computer in BEATS[player]:
-        prompt("You win!")
-    elif computer in LOSES_TO[player]:
-        prompt("Computer wins!")
+        return "player"
+    if computer in LOSES_TO[player]:
+        return "computer"
+
+    return "tie"
+
+
+def display_winner(player: str, computer: str):
+    prompt(f"You chose {player}. Computer chose {computer}.")
+    winner = determine_winner(player, computer)
+
+    if winner == "player":
+        prompt("You win!\n")
+    elif winner == "computer":
+        prompt("Computer wins.\n")
     else:
-        prompt("It's a tie!")
+        prompt("It's a tie.\n")
 
 
-def play_rps():
+def display_header(player_score: int, computer_score: int):
+    print("-" * 55)
+    print(
+        f"|        Player: {player_score}        |"
+        + f"        Computer: {computer_score}        |"
+    )
+    print("-" * 55)
+
+
+def display_match_winner(scores: dict):
+    print("*" * 55)
+    print(f"You won {scores["player"]}.")
+    print(f"Computer won {scores["computer"]}.\n")
+
+    if scores["player"] >= scores["computer"]:
+        print("You win the match!")
+    else:
+        print("Computer wins the match.")
+
+    print("*" * 55)
+
+
+def play_game() -> str:
+    prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
+    player_choice = process_choice(input())
+
+    while player_choice not in VALID_CHOICES:
+        prompt("That's not a valid choice")
+        player_choice = process_choice(input())
+
+    computer_choice = random.choice(VALID_CHOICES)
+    display_winner(player_choice, computer_choice)
+    return determine_winner(player_choice, computer_choice)
+
+
+def play_match():
+    scores = {"player": 0, "computer": 0}
+    while all(score < 5 for score in scores.values()):
+        display_header(scores["player"], scores["computer"])
+        winner = play_game()
+        if winner in scores:
+            scores[winner] += 1
+
+    display_match_winner(scores)
+
+
+### Main Loop
+while True:
+    play_match()
+    prompt("Would you like to play another match? (y/n)?")
+    answer = input().lower()
     while True:
-        prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
-        choice = process_choice(input())
-
-        while choice not in VALID_CHOICES:
-            prompt("That's not a valid choice")
-            choice = process_choice(input())
-
-        computer_choice = random.choice(VALID_CHOICES)
-        display_winner(choice, computer_choice)
-
-        prompt("Do you want to play again (y/n)?")
-        answer = input().lower()
-        while True:
-            if answer.startswith("n") or answer.startswith("y"):
-                break
-
-            prompt('Please enter "y" or "n".')
-            answer = input().lower()
-
-        if answer[0] == "n":
+        if answer.startswith("n") or answer.startswith("y"):
             break
 
+        prompt('Please enter "y" or "n".')
+        answer = input().lower()
 
-play_rps()
+    if answer[0] == "n":
+        break
